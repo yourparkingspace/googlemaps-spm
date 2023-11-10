57ddbfceeb2d54fbbd4cfef1e0e77c2e694331c5
David John Smailes <david.smailes@gmail.com>
2023-11-10 09:39:23 +0000

    MOB-1898: update checksums

--- Package.swift
+++ Package.swift
@@ -43,28 +43,28 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "GoogleMaps",
-                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/7.1.0/GoogleMaps.xcframework.zip",
-                    checksum: "825102248e774c68c330cad1873834864ad152441bb36effbcb23993d1e1da4a"
+                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/8.1.0/GoogleMaps.xcframework.zip",
+                    checksum: "b6bbad8d62bdebdac4ac519462dde6ab068a53ac847951b8f9c63dd349a19ace"
                 ),
         .binaryTarget(
                     name: "GoogleMapsBase",
-                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/7.1.0/GoogleMapsBase.xcframework.zip",
-                    checksum: "485e5123233f682befcd6e9e13c7452abdcbc2992dedad1bbb9326a541af5e04"
+                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/8.1.0/GoogleMapsBase.xcframework.zip",
+                    checksum: "a95ee5489916c5053d54b5b7f1cc85f78dc4297b6ff3660b405f21fe43d041e0"
                 ),
         .binaryTarget(
                     name: "GoogleMapsCore",
-                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/7.1.0/GoogleMapsCore.xcframework.zip",
-                    checksum: "77123c2d929d22502498e0bf1581f32355e4bd1e8211ef288c7869e981051002"
+                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/8.1.0/GoogleMapsCore.xcframework.zip",
+                    checksum: "a1081239d7692a159295510553194c8aa97bfd49ed73a4e3ba6e95099b9e629a"
                 ),
         .binaryTarget(
                     name: "GoogleMapsM4B",
-                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/7.1.0/GoogleMapsM4B.xcframework.zip",
-                    checksum: "e6290834d28f6a3ead9180bf83f85b4fe5954e1c20efd6f5eaffafbcbfc6a390"
+                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/8.1.0/GoogleMapsM4B.xcframework.zip",
+                    checksum: "d261f63023586486ff7dffbe02a67e3985f92c4b7647b629566c948134f3164a"
                 ),
         .binaryTarget(
                     name: "GooglePlaces",
-                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/7.1.0/GooglePlaces.xcframework.zip",
-                    checksum: "f03f9ea1e7357df6b42b41e8fe8ca9c7b28c05f9a0305dab4997cc8b04d61503"
+                    url: "https://github.com/yourparkingspace/googlemaps-spm/releases/download/8.1.0/GooglePlaces.xcframework.zip",
+                    checksum: "0d6eb6ba6da31203b9d5a0e57fb28b6b4321d3bde2db0014b830121fb0af065b"
                 )
     ]
 )
